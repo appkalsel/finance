a0ae91d625c162fa4443e0969b04931654e7519b
appkalsel <appkalsel@gmail.com>
2023-01-20 01:33:46 +0800

    upload file
    upload file

---
+++ application/controllers/Auth.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		if ($this->session->userdata('login')) {
+			redirect(base_url('Home'));
+		}
+	}
+
+	public function index()
+	{
+		$this->load->view('auth_login');
+	}
+
+	public function auth_login()
+	{
+		$email = htmlspecialchars($this->input->post('email', TRUE));
+		$password = htmlspecialchars($this->input->post('pw', TRUE));
+
+		$data = $this->m_auth->auth($email);
+		// var_dump($cek['pw']);
+		if ($data) {
+			# code...
+			if (password_verify($password, $data['pw'])) {
+				if ($data['is_active'] != 2) {
+					$this->session->set_flashdata('alert', 'error');
+					redirect('Welcome');
+				} else {
+					$this->session->set_userdata('login', TRUE);
+					if ($data['id_role'] === '1') { //Akses admin
+						$this->session->set_userdata('akses', 'Administrator');
+						$this->session->set_userdata('ses_id_users', $data['id']);
+						$this->session->set_userdata('ses_nama', $data['name']);
+						$this->session->set_userdata('ses_email', $data['email']);
+						$this->session->set_userdata('ses_foto', $data['image']);
+					} elseif ($data['id_role'] === '2') { //akses Owner
+						$this->session->set_userdata('akses', 'Owner');
+						$this->session->set_userdata('ses_id_users', $data['id']);
+						$this->session->set_userdata('ses_nama', $data['name']);
+						$this->session->set_userdata('ses_email', $data['email']);
+						$this->session->set_userdata('ses_foto', $data['image']);
+					}
+					$this->session->set_flashdata('alert', 'login');
+					redirect('Admin');
+				}
+			} else {
+				$this->session->set_flashdata('alert', 'error');
+				redirect('Auth');
+			}
+		} else {
+			$this->session->set_flashdata('alert', 'error');
+			redirect('Auth');
+		}
+	}
+
+	public function logout()
+	{
+		session_destroy();
+		redirect(base_url());
+	}
+}
---
+++ application/views/file/footer.php
@@ -0,0 +1,10 @@
+<script src="<?= base_url() ?>vendors/popper/popper.min.js"></script>
+<script src="<?= base_url() ?>vendors/bootstrap/bootstrap.min.js"></script>
+<script src="<?= base_url() ?>vendors/anchorjs/anchor.min.js"></script>
+<script src="<?= base_url() ?>vendors/is/is.min.js"></script>
+<script src="<?= base_url() ?>vendors/fontawesome/all.min.js"></script>
+<script src="<?= base_url() ?>vendors/lodash/lodash.min.js"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+<script src="<?= base_url() ?>vendors/prism/prism.js"></script>
+<script src="<?= base_url() ?>vendors/swiper/swiper-bundle.min.js"></script>
+<script src="<?= base_url() ?>assets/js/theme.js"></script>
